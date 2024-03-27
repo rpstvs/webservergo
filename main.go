8864d71dd1d477ade797254e78a7e70100c8b520
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-03-27 22:22:39 +0000

    added a few lines to start the server

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import "net/http"
+
 func main() {
 
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
