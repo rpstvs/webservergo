c5fb7134282e8be8bf617615c5d812e4aab08a64
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-03-29 21:19:45 +0000

    new handlers

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import "net/http"
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
-
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
 
 	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", apiCfg.midlewareMetricsInc(mux))
 	mux.HandleFunc("/healthz", healthHandler)
 
 	corsMux := middlewareCors(mux)
@@ -27,6 +32,10 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func counterHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -36,6 +45,14 @@ func middlewareCors(next http.Handler) http.Handler {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) midlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
 	})
 }
