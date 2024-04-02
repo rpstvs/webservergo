eeb2d72e96f9229ddaf2f26aaa9683a4af6407a5
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-04-02 12:34:01 +0100

    added new type

--- validation.go
+++ validation.go
@@ -1,10 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
 
 	type parameters struct {
 		Body string `json:"body"`
 	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		log.Printf("Error encoding parameters %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
 }
