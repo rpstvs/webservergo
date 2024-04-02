47931ee8a7495f9c5c0fec1eb858912b2d23d15c
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-04-02 21:57:52 +0100

    finished validation

--- validation.go
+++ validation.go
@@ -23,9 +23,36 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&params)
 
 	if err != nil {
-		log.Printf("Error encoding parameters %s", err)
-		w.WriteHeader(500)
+		respondwithError(w, http.StatusInternalServerError, "couldn't decode parameters")
 		return
 	}
 
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondwithError(w, 400, "Chirp is too long")
+	}
+	respondwithJSON(w, http.StatusOK, returnVals{Valid: true})
+
+}
+
+func respondwithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5xx error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondwithJSON(w, code, errorResponse{Error: msg})
+}
+
+func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshaling json: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
