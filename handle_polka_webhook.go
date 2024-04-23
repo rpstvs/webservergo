2f39c51e883cdecc2072c57303d6df89468f9d32
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-04-23 22:06:16 +0100

    Added Handler for webhook

--- handle_polka_webhook.go
+++ handle_polka_webhook.go
@@ -1,7 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func (cfg *apiConfig) PolkaHandler(w http.ResponseWriter, r *http.Request) {
 
+	type PaymentEvent struct {
+		UserID int `json:"user_id"`
+	}
+	type Body struct {
+		Data  PaymentEvent `json:"data"`
+		Event string       `json:"event"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := Body{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondwithError(w, http.StatusInternalServerError, "couldnt decode")
+	}
+	fmt.Println(params.Event)
 }
