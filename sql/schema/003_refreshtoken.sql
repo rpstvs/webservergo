c9a38ba4c39f5ecbe059b8690989ef250a069dd8
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-05-09 12:31:24 +0100

    added queries

---
+++ sql/schema/003_refreshtoken.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE refresh_tokens(
+    token TEXT NOT NULL PRIMARY KEY,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    user_id UUID NOT NULL REFERENCES users(id),
+    expires_at TIMESTAMP,
+    revoked_at TIMESTAMP
+) -- goose Down
+DROP TABLE refresh_tokens
